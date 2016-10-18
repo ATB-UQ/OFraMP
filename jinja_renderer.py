8b7c2dcf22a655582f0dad1cd00f99cf54f63c48
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-10-19 09:16:28 +1000

    Feature: Add Google Analytics

--- jinja_renderer.py
+++ jinja_renderer.py
@@ -2,6 +2,7 @@ from os.path import join
 from jinja2 import Template
 
 BASE_SERVER = 'http://fragments.atb.uq.edu.au'
+GOOGLE_ANALYTICS_ID = 'UA-59222530-5'
 
 PROJECT_NAME_FORMATTER = lambda project_name: project_name.lower()
 
@@ -13,6 +14,7 @@ if __name__ == '__main__':
             rendered_view = Template(fh.read()).render(
                 base_server=BASE_SERVER,
                 project_name_formatter=PROJECT_NAME_FORMATTER,
+                google_analytics_id=GOOGLE_ANALYTICS_ID,
             )
         with open(join(SOURCE_DIR, template.replace('.epy', '')), 'w') as fh:
             fh.write(rendered_view)
